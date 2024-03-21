af1db7e29f5b8910be74510f3a886f383b535c70
ayush6999 <ayushjaiswal6999@gmail.com>
2024-03-21 14:47:20 +0530

    intigrate Primsa

--- src/index.ts
+++ src/index.ts
@@ -2,20 +2,66 @@ import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
-import dbConnect from './database/dbConnect';
-
 dotenv.config();
-
 const PORT = process.env.PORT || 3000;
-const DB_URL = process.env.DATABASE_URL || 'your mongodb url'
-
 const app = express();
+
+import { PrismaClient } from '@prisma/client'
+const prisma = new PrismaClient()
+
+
+async function createUser() {
+  await prisma.user.create({
+
+    data: {
+
+      name: 'Alice',
+
+      email: 'alice@prisma.io',
+
+      posts: {
+
+        create: { title: 'Hello World' },
+
+      },
+
+      profile: {
+
+        create: { bio: 'I like turtles' },
+
+      },
+
+    },
+
+  })
+}
+
+
+createUser();
+
+
+async function getUser() {
+  // ... you will write your Prisma Client queries here
+  const allUsers = await prisma.user.findMany()
+  console.log(allUsers)
+}
+getUser()
+.then(async () => {
+  await prisma.$disconnect();
+})
+.catch(async () => {
+  await prisma.$disconnect();
+})
+
+
+
+
+
 
 // Middleware
 app.use(cors());
 app.use(bodyParser.json());
 
-dbConnect(DB_URL)
 
 // Routes
 app.get("/", (req, res) => {
