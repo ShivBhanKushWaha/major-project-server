878075bf0d0ed851d7b5f986cfd58b57f93feca2
ShivBhanKushWaha <shivbhankushwaha862003672001@gmail.com>
2024-05-22 15:05:55 +0530

    Port Changed

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,7 @@ import cors from 'cors';
 import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 dotenv.config();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 const app = express();
 
 
