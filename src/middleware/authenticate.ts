58d6fcfb20f9902f980a3e3565f9796f3e7d76ab
ShivBhanKushWaha <shivbhankushwaha862003672001@gmail.com>
2024-06-08 18:48:59 +0530

    Header

--- src/middleware/authenticate.ts
+++ src/middleware/authenticate.ts
@@ -2,7 +2,7 @@ import { verifyToken } from './jwt';
 import express from 'express';
 
 const authenticate = (req : any, res : any, next : any) => {
-  const token = req.headers.authorization?.split(' ')[1];
+  const token = req.headers.authorization;
 
   if (!token) {
     return res.status(401).json({ message: 'Access denied. No token provided.' });
